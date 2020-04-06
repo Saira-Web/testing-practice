9177e2502dc1da049985074f5a92617c14c83c91
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-04-06 01:35:45 -0400

    Test Case 2 implemented

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -16,11 +16,13 @@ try {
   // It should return the sum of two numbers.
   var result = sum(1, 1);
   if (result !== 2) throw new Error('Expected sum(1, 1) to be 2. Received: ' + result);
-
+        
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = sum(1, 1, 1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
